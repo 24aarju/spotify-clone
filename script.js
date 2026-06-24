fd1ae43500a38583202add60256c539246f2de06
24aarju <aarjupaswan11th@gmail.com.>
2026-06-24 06:05:45 -0900

    added songs in my project through java script

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("Let's write JavaScript");
+
+async function getSongs() {
+
+    let a = await fetch("http://127.0.0.1:5500/songs/");
+    let response = await a.text();
+
+    let div = document.createElement("div");
+    div.innerHTML = response;
+
+    let as = div.getElementsByTagName("a");
+
+    let songs = [];
+
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href);
+        }
+    }
+
+    return songs;
+}
+
+async function main() {
+    //get the list of all the songs
+    let songs = await getSongs();
+    songs.forEach(song => {
+    console.log(song);
+});
+
+//play the first song
+let audio = new Audio(songs[0]);
+
+document.body.addEventListener("click", () => {
+    audio.play();
+});
+
+
+
+}
+
+main();
